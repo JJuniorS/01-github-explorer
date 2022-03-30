480f644b6f1df351010980388205a91ebc13ab7a
JJuniorS <myannotation.dev@gmail.com>
2022-03-29 21:09:20 -0300

    fast refresh error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-
-    return <h1>Hello WOrld App React</h1>
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export function Counter(){
+    const [counter, setCounter] = useState(0);
+
+    function increment(){
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button type="butonn" onClick={increment}>
+                Increment + 2
+            </button>
+        </div>
+    );
+}
